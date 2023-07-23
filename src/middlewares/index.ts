01eeb878ed6061d1f06d2788fd403a82d6e5adab
alamiali12 <139745680+alamiali12@users.noreply.github.com>
2023-07-23 09:56:08 +0330

    new

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,12 +1,33 @@
-import  express  from "express";
+import express from "express";
 import { get, merge } from "lodash";
 
 import { getUserBySessinToken } from "../db/users";
 
-export const isAuthenticated =async (req:express.Request, res:express.Response, next:express.NextFunction) => {
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, 'identity._id') as unknown as string;
+
+        if (!currentUserId) {
+            return res.sendStatus(403);
+        }
+
+        if (currentUserId.toString() !== id) {
+            return res.sendStatus(400);
+        }
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
 
-        const sessionToken = req.cookies ['ALAMI_A'];
+        const sessionToken = req.cookies['ALAMI_A'];
 
         if (!sessionToken) {
             return res.sendStatus(403);
@@ -14,15 +35,15 @@ export const isAuthenticated =async (req:express.Request, res:express.Response, 
 
         const existingUser = await getUserBySessinToken(sessionToken);
 
-        if( !existingUser) {
+        if (!existingUser) {
             return res.sendStatus(403)
         }
 
-        merge(req , {identity: existingUser });
+        merge(req, { identity: existingUser });
 
         return next();
 
-    }catch (error) {
+    } catch (error) {
         console.log(error);
         return res.sendStatus(400);
     }
